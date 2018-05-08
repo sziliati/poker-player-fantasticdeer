7e47bef0c5cdf95bdd4c3d88ca5c08c8e5fcba56
sagi.kazar.mark <sagi.kazar.mark@ustream.tv>
2018-05-08 10:12:59 +0200

    Fix betting

--- player.php
+++ player.php
@@ -8,11 +8,13 @@ class Player
     {
     	file_put_contents("php://stderr", var_export($game_state, true)."\n");
 
-		if ($game_state['current_buy_in'] > 20) {
+		if ($game_state['current_buy_in'] > 50) {
 			return 0;
 		}
 
-        return $game_state['small_blind'];
+		$bet = $game_state['current_buy_in'] - $game_state[$game_state['in_action']]['bet'];
+
+        return max($bet, $game_state['small_blind']);
     }
 
     public function showdown($game_state)
