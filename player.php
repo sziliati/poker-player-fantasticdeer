1203c14178cdce70afa171e749a679c1509ada1f
sagi.kazar.mark <sagi.kazar.mark@ustream.tv>
2018-05-08 10:06:40 +0200

    Fix logging

--- player.php
+++ player.php
@@ -6,7 +6,7 @@ class Player
 
     public function betRequest($game_state)
     {
-    	file_put_contents(STDOUT, var_export($game_state, true));
+    	file_put_contents("php://stderr", var_export($game_state, true)."\n");
 
 		if ($game_state['current_buy_in'] > 20) {
 			return 0;
