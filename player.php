dc918f4230448b17130cceb631856b32000ea2b2
sagi.kazar.mark <sagi.kazar.mark@ustream.tv>
2018-05-08 10:05:08 +0200

    Stop spending too much money

--- player.php
+++ player.php
@@ -8,6 +8,10 @@ class Player
     {
     	file_put_contents(STDOUT, var_export($game_state, true));
 
+		if ($game_state['current_buy_in'] > 20) {
+			return 0;
+		}
+
         return $game_state['small_blind'];
     }
 
